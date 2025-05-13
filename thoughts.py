ce381c467e7d1d915be8d27935b2cee1207d51ab
DanMGocan <dangocan@outlook.com>
2025-05-13 14:41:11 +0100

    UI improvements

--- thoughts.py
+++ thoughts.py
@@ -1,4 +1,5 @@
 ################## TODO ######################
+## Fix registration emails not sent 
 # We need to remove the email address from the Dashboard and instead, cut the email address just before the @ so it still displays the name of the user. 
 # Do not forget to replace admin@slidepull.net with admin@slidepull.com
 # Cron jobs to clear the blob and DB of data no longer in use 
